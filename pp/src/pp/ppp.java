da9beacf16585722aa97ab4467f917ead9a61f5b
Shree <Shree@Shree-PC>
2017-09-28 11:29:15 +0530

    pp commit

---
+++ pp/src/pp/ppp.java
@@ -0,0 +1,19 @@
+package pp;
+
+import java.util.Scanner;
+
+public class ppp {
+	
+	public static void main(String args[])
+	{
+		int a, b, c;
+		System.out.println("Enter the two numbers:-");
+		Scanner in=new Scanner(System.in);
+		a=in.nextInt();
+		b=in.nextInt();
+		c = a + b;
+		System.out.println("sum:="+c);
+		
+	}
+
+}
